d127c30d3e4383918e2b216d7c5cbe670855b4f6
danielmlduarte <danielduarte.tryber@gmail.com>
2021-01-19 00:12:36 -0300

    Exercise 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redCar: false,
+      blueCar: false,
+      yellowCar: false,
+    };
+    this.handleCarMove = this.handleCarMove.bind(this);
+  }
+
+  handleCarMove(name, value) {
+    this.setState({
+      [name]: value, 
+    });
+  }
+
+  render() {
+    const contextValue = {
+      ...this.state,
+      carMove: this.handleCarMove,
+    }
+    return (
+      <MyContext.Provider value={ contextValue } >
+        <div>
+          <Cars />        
+        </div>
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
